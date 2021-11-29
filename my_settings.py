c7c0cf3815b8799fb1ad2f2a92d443ee11bed0f1
jaemoon <jaemoon406@gmail.com>
2021-11-29 18:30:37 +0900

    [Modify] add requrirements.txt change (mysettings > my_settings)

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = 'django-insecure-ij_g1y84_qvgcoo$n+bfaa7n*!(e4g&hf%&xo1bnjz4(02%a*^'
+
+DATABASES  = {
+    'default' : {
+        'ENGINE'  : 'django.db.backends.mysql',
+        'NAME'    : 'bagettt',
+        'USER'    : 'root',
+        'PASSWORD': 'Dlwoans12!',
+        'HOST'    : '127.0.0.1',
+        'PORT'    : '3306',
+        'OPTIONS' : {'charset': 'utf8mb4'}
+    }
+}
+
