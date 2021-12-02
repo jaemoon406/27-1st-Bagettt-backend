adfadf9125aab2ccd14f4a4f3ba3e408bed554ff
lilo <eunehghim@gmail.com>
2021-12-02 14:25:44 +0900

    [Add] db_uploader script

--- bagettt/settings.py
+++ bagettt/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-from my_settings import SECRET_KEY, DATABASES
+import pymysql
+
+from pathlib        import Path
+from my_settings    import SECRET_KEY, DATABASES
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+pymysql.install_as_MySQLdb() 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'packages',
     'users',
     'shops',
+    'core'
 ]
 
 MIDDLEWARE = [
