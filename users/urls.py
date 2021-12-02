241e06d9cdbf6d5ccf6a137a46136c3063b3371a
jaemoon <jaemoon406@gmail.com>
2021-12-02 16:35:05 +0900

    [Working] commit for change models

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+
+from users.views import SignupView
 
 urlpatterns = [
+    path('/signup',SignupView.as_view()),
+    # path('/signin',SigninView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class SignupView(View):
+    def post(self,request):
+        try:
+            data = json.loads(request.body)
+            email_regex    = r'^[a-zA-Z0-9.-_]+\@[a-zA-Z0-9.-]+\.[a-zA-Z]+$'
+            password_regex = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}'
+
+            if not re.match(email_regex,data['email']):
+                return JsonResponse({'message':'INVALID_EMAIL'}, status=201)
+            if not re.match(password_regex,data['password']):
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status=201)
+            User.objects.create(
+                email     = data['email'],
+                password  = data['password'],
+                name      = data['name'],
+                address   = data['address'],
+                phone_number = data['phone_number']
+                    )
+            return JsonResponse({'message':'Congratulation'}, status=201)
+        except: 
+            return JsonResponse({'message':'ACCOUNT_EXISTS'})
