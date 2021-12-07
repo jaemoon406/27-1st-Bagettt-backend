a3be47888d0c9513cd5ca7f98bc97eb6d80ecadf
lilo <eunehghim@gmail.com>
2021-12-08 00:20:34 +0900

    [Modify] packages_view

--- packages/views.py
+++ packages/views.py
@@ -8,15 +8,15 @@ from packages.models            import Package, Product
 class PackagesListView(View):
     def get(self, request):   
         
+        brand     = request.GET.getlist('brand', None) 
+        ordering  = request.GET.get('order', 'id')
+
         q = Q()
-        
-        brand_name = request.GET.get('brand_name', None)
-        ordering   = request.GET.get('order', 'id')
 
-        if brand_name:
-            q &= Q(products__brand_name=brand_name)
-
-        packages = Package.objects.filter(q).order_by(ordering)
+        if brand:
+            q &= Q(products__brand_name__in=brand)
+            
+        packages = Package.objects.filter(q).order_by(ordering).distinct()
         
         results = [{
             "id"    : package.id,
@@ -40,18 +40,16 @@ class ProductsView(View):
                 "package_description"  : package.description,
                 "package_thumbnail"    : package.thumbnail_image,
                 "price"                : package.price,
-                
-                "product_details"      : [
-                        {   "name"      : product.name,
+                "product_details"      : [{   
+                            "name"      : product.name,
                             "brand_name": product.brand_name,
                             "image"     : product.image_url,
                             "kcal"      : product.kcal,
                             "nutrition" : product.nutrition
                     } for product in products],
-                }
-            ]
+                }]
         
             return JsonResponse({'result':result}, status=200)
         
         except Package.DoesNotExist:
-            return JsonResponse({'messages':'NOT_FOUND'}, status=401)
+            return JsonResponse({'messages':'NOT_FOUND'}, status=404) 
